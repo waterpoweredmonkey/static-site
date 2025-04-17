4b4ebaba91615c6121f67c22aef8fc1f0249c16e
Matthew W Burnard <oldenglandimmigrant@gmail.com>
2025-04-16 22:18:35 -0700

    CH5: Website, L1: Copy Static.

--- src/main.py
+++ src/main.py
@@ -1,13 +1,43 @@
+import os
+import shutil
+
 from textnode import TextNode, TextType
 
+def copy_directory(src, dest):
+    print(f"Copying directory: {src} to {dest}")
+    if not os.path.exists(dest):
+        os.mkdir(dest)
+    for filename in os.listdir(src):
+        src_path = os.path.join(src, filename)
+        dest_path = os.path.join(dest, filename)
+        try:
+            if os.path.isfile(src_path):
+                print(f"Copying file: {src_path} to {dest_path}")
+                shutil.copy(src_path, dest_path)
+            elif os.path.isdir(src_path):
+                copy_directory(src_path, dest_path)
+        except Exception as e:
+            print('Failed to copy %s. Reason: %s' % (src_path, e))
+
+
+def delete_directory(path):
+    for filename in os.listdir(path):
+        file_path = os.path.join(path, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                print(f"Deleting file: {file_path}")
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                print(f"Deleting directory: {file_path}")
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+def refresh_public():
+    delete_directory('./public')
+    copy_directory('./static', './public')
+
 def main():
-    node = TextNode(
-        "This is a text node",
-        TextType.BOLD,
-        "https://www.boot.dev"
-    )
-    print(node)
-
-
+    refresh_public()
 
 main()
